b7a32eeb5529e982bf4b0b40bcaf47d6c371aa60
Swaraj Rath <gulal009@gmail.com>
2020-11-26 20:39:57 +0100

    mongoose setup

---
+++ db/mongoose.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const keys = require("./../config/keys");
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(keys.MONGO_URI, {
+	useNewUrlParser: true,
+	useCreateIndex: true
+});
+
+mongoose.connection.on("connected", () => {
+	console.log("Connected to Mongo instance");
+});
+
+mongoose.connection.on("error", error => {
+	console.log("Error connecting to Mongo instance", error);
+});
