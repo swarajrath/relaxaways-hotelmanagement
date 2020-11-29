bcd9aec2516aa0d4fed9373d3673cc4728ef8b03
Swaraj Rath <gulal009@gmail.com>
2020-11-29 18:05:25 +0100

    Add HotelForms

---
+++ relaxaways-frontend/src/Components/AddHotel/AddHotelForm.js
@@ -0,0 +1,333 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link, withRouter } from 'react-router-dom';
+import * as actions from '../../Redux/actions';
+import { Hotel } from '../../types';
+
+// Components
+import { TEXT_FIELDS, NUMBER_FIELDS, BOOLEAN_FIELDS } from './formFields';
+import ToggleSwitch from '../External/ToggleSwitch/ToggleSwitch';
+import DropzoneElement from '../External/Dropzone/DropzoneElement';
+import Notification from '../External/Notification/Notification';
+
+// interface IState {
+//   files: Array;
+//   fields: Partial;
+//   errors: any;
+//   hotel_images: any;
+// }
+
+// interface IProps {
+//   addHotel: (field: any) => void;
+//   history: any;
+// }
+
+enum ErrorMessageEnum {
+  provideValue = 'FAILURE! You must provide a value',
+  empty = 'Cannot be empty',
+}
+
+class AddHotelForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      files: [],
+      fields: {
+        is_new: false,
+        is_apartment: false,
+        facilities_restaurant: false,
+        facilities_gym: false,
+        facilities_wifi: false,
+        facilities_card_payment: false,
+        facilities_game_room: false,
+        hotel_images: [],
+      },
+      hotel_images: [],
+      errors: {},
+    };
+  }
+
+  handleValidation = () => {
+    let fields = this.state.fields;
+    let errors = {};
+    let formIsValid = true;
+
+    TEXT_FIELDS.forEach(({ name }) => {
+      if (!fields[name]) {
+        formIsValid = false;
+        errors[name] = ErrorMessageEnum.empty;
+      }
+    });
+
+    NUMBER_FIELDS.forEach(({ name }) => {
+      if (!fields[name]) {
+        formIsValid = false;
+        errors[name] = ErrorMessageEnum.empty;
+      }
+    });
+
+    this.setState({ errors: errors });
+    return formIsValid;
+  };
+
+  handleChange = (field, e) => {
+    let fields = this.state.fields;
+    fields[field] = e.target.value;
+    this.setState({ fields });
+    this.handleValidation();
+  };
+
+  renderTextFields = () => {
+    return (
+      <div className="hotel-form__formValueBox">
+        <div>
+          <label htmlFor="">Hotel name</label>
+          <input
+            type="text"
+            placeholder="Burj Al Arab"
+            onChange={this.handleChange.bind(this, 'hotel_name')}
+            value={this.state.fields['hotel_name'] || ''}
+            className={
+              this.state.errors['hotel_name'] ? 'input input__error' : 'input'
+            }
+          />
+        </div>
+        <div>
+          <label htmlFor="">Hotel adress</label>
+          <input
+            type="text"
+            placeholder="Jumeirah St - Dubai"
+            onChange={this.handleChange.bind(this, 'hotel_adress')}
+            value={this.state.fields['hotel_adress'] || ''}
+            className={
+              this.state.errors['hotel_adress'] ? 'input input__error' : 'input'
+            }
+          />
+        </div>
+        <div>
+          <label htmlFor="">Hotel city</label>
+          <input
+            type="text"
+            placeholder="Dubai"
+            onChange={this.handleChange.bind(this, 'hotel_city')}
+            value={this.state.fields['hotel_city'] || ''}
+            className={
+              this.state.errors['hotel_city'] ? 'input input__error' : 'input'
+            }
+          />
+        </div>
+        <div>
+          <label htmlFor="">Hotel province</label>
+          <input
+            type="text"
+            placeholder={'Jumeirah'}
+            onChange={this.handleChange.bind(this, 'hotel_province')}
+            value={this.state.fields['hotel_province'] || ''}
+            className={
+              this.state.errors['hotel_province']
+                ? 'input input__error'
+                : 'input'
+            }
+          />
+        </div>
+        <div>
+          <label htmlFor="">Hotel description</label>
+          <input
+            type="text"
+            placeholder={'This is the best hotel on the world'}
+            onChange={this.handleChange.bind(this, 'hotel_description')}
+            value={this.state.fields['hotel_description'] || ''}
+            className={
+              this.state.errors['hotel_description']
+                ? 'input input__error'
+                : 'input'
+            }
+          />
+        </div>
+      </div>
+    );
+  };
+
+  renderNumberFields = () => {
+    return (
+      <div className="hotel-form__formValueBox">
+        <div>
+          <label htmlFor="">Hotel price</label>
+          <input
+            type="number"
+            min={0}
+            placeholder={'172'}
+            onChange={this.handleChange.bind(this, 'hotel_price')}
+            value={this.state.fields['hotel_price'] || 0}
+            className={
+              this.state.errors['hotel_price'] ? 'input input__error' : 'input'
+            }
+          />
+        </div>
+        <div>
+          <label htmlFor="">Hotel distance</label>
+          <input
+            type="number"
+            min={0}
+            placeholder={'10'}
+            onChange={this.handleChange.bind(this, 'hotel_distance')}
+            value={this.state.fields['hotel_distance'] || 0}
+            className={
+              this.state.errors['hotel_distance']
+                ? 'input input__error'
+                : 'input'
+            }
+          />
+        </div>
+        <div>
+          <label htmlFor="">Hotel stars</label>
+          <input
+            type="number"
+            min={0}
+            placeholder={'5'}
+            onChange={this.handleChange.bind(this, 'hotel_stars')}
+            value={this.state.fields['hotel_stars'] || 0}
+            className={
+              this.state.errors['hotel_stars'] ? 'input input__error' : 'input'
+            }
+          />
+        </div>
+        <div>
+          <label htmlFor="">Hotel rating</label>
+          <input
+            type="number"
+            min={0}
+            placeholder={'8.9'}
+            onChange={this.handleChange.bind(this, 'hotel_rating')}
+            value={this.state.fields['hotel_rating'] || 0}
+            className={
+              this.state.errors['hotel_rating'] ? 'input input__error' : 'input'
+            }
+          />
+        </div>
+        <div>
+          <label htmlFor="">Hotel reviews</label>
+          <input
+            type="number"
+            min={0}
+            placeholder={'1200'}
+            onChange={this.handleChange.bind(this, 'hotel_reviews')}
+            value={this.state.fields['hotel_reviews'] || 0}
+            className={
+              this.state.errors['hotel_reviews']
+                ? 'input input__error'
+                : 'input'
+            }
+          />
+        </div>
+      </div>
+    );
+  };
+
+  setToggleValues = (value = false, id) => {
+    let fields = this.state.fields;
+    fields[id] = value;
+    this.setState({ fields });
+  };
+
+  renderBooleanFields = () => {
+    return (
+      <div className="hotel-form__formValueBox">
+        {BOOLEAN_FIELDS.map(({ label, name }, i) => {
+          return (
+            <div className={'hotel-form__toggleInput'} key={i}>
+              <ToggleSwitch
+                onClick={this.setToggleValues}
+                theme={'default'}
+                className={'d-flex'}
+                label={label}
+                id={name}
+              />
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (this.handleValidation()) {
+      this.props.addHotel(this.state.fields);
+      console.log(this.state.fields);
+      this.props.history.push('/send');
+    } else {
+      return;
+    }
+  };
+
+  onDrop = (files) => {
+    this.setState({ files });
+    this.encodeImageFileAsURL(files);
+  };
+
+  encodeImageFileAsURL = (elements) => {
+    let fields = this.state.fields;
+    fields['hotel_images'] = elements;
+    this.setState({
+      fields,
+    });
+  };
+
+  render() {
+    const isErrors = Object.keys(this.state.errors).length === 0;
+
+    return (
+      <div className={'hotel-form'}>
+        {isErrors !== true && (
+          <Notification type={'danger'} text={ErrorMessageEnum.provideValue} />
+        )}
+        <div className="hotel-form__container">
+          <div className={'content__header-wrapper'}>
+            <h1 className={'view-header'}>Fill to submit</h1>
+          </div>
+          <form
+            onSubmit={this.handleSubmit}
+            className="hotel-form__form"
+            encType="multipart/form-data"
+            method="post"
+            id={'test-form'}>
+            <div className="hotel-form__formValuesBox">
+              {this.renderTextFields()}
+              {this.renderNumberFields()}
+              {this.renderBooleanFields()}
+            </div>
+
+            <div className={'content__header-wrapper'}>
+              <h1 className={'view-header'}>Add files</h1>
+            </div>
+            <div className={'hotel-form__dropzone'}>
+              <DropzoneElement onDrop={this.onDrop} files={this.state.files} />
+              <div className="hotel-form__buttons">
+                <Link to="/" className="button is-danger">
+                  Cancel
+                </Link>
+                <button type="submit" className="button is-info">
+                  Next
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    addHotelFormValues: state.addHotelFormValues,
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  actions,
+)(withRouter(AddHotelForm));
