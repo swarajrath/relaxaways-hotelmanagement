6f8547d7cc3dceb404eeab7fa3f90305a21cadce
Swaraj Rath <gulal009@gmail.com>
2020-11-30 20:06:27 +0100

    Implementing box

---
+++ relaxaways-frontend/src/Components/Box/Box.js
@@ -0,0 +1,20 @@
+import React, { PureComponent } from 'react';
+import { Props } from './types';
+
+class Box extends PureComponent {
+  render() {
+    return (
+      <div key={this.props.index} className={`box box--${this.props.index}`}>
+        <div className={'box__content'}>
+          <div className={`box__icon box__icon--${this.props.index}`} />
+          <div className={'box__text'}>
+            <span className={'box__text--value'}>{this.props.value}</span>
+            <span className={'box__text--title'}>{this.props.title}</span>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Box;
