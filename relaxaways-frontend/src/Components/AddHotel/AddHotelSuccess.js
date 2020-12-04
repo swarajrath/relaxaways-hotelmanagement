6a5b9187dca09645ee4333e11bdbcd8c4d6c2c23
Swaraj Rath <gulal009@gmail.com>
2020-12-04 17:48:49 +0100

    Success after adding hotel

---
+++ relaxaways-frontend/src/Components/AddHotel/AddHotelSuccess.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import Notification from '../External/Notification/Notification.tsx';
+
+class AddHotelSuccess extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalIsOpen: true,
+    };
+    this.onModalOpen = this.onModalOpen.bind(this);
+    this.onCloseModal = this.onCloseModal.bind(this);
+  }
+
+  onModalOpen() {
+    this.setState({ modalIsOpen: true });
+  }
+
+  onCloseModal() {
+    this.setState({ modalIsOpen: false });
+  }
+
+  componentDidMount() {
+    this.props.history.push('/');
+  }
+
+  render() {
+    return (
+      <Notification
+        type={'success'}
+        text={'The operation ended with success'}
+      />
+    );
+  }
+}
+
+export default AddHotelSuccess;
