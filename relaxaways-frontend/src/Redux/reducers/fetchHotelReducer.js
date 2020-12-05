41b5808f30287b5f559000b4734a5f5baa996b63
Swaraj Rath <gulal009@gmail.com>
2020-12-05 18:56:41 +0100

    Fetch hotel redux

---
+++ relaxaways-frontend/src/Redux/reducers/fetchHotelReducer.js
@@ -0,0 +1,10 @@
+import * as types from "../../types";
+
+export default (state = [], action) => {
+  switch (action.type) {
+    case types.FETCH_HOTELS:
+      return action.payload || false;
+    default:
+      return state;
+  }
+}
