b0dc1898d61b413440e7e57afbe936bad4a1cf87
Swaraj Rath <gulal009@gmail.com>
2020-12-08 10:36:39 +0100

    Details of Hotel

---
+++ relaxaways-frontend/src/Components/Hotel/HotelDetails/HotelDetails.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import HotelFacilities from "../HotelFacilities/HotelFacilities";
+
+export const HotelDetails = (props) => {
+	return (
+		<div className={"details"}>
+			<div className="content__header-wrapper">
+				<h1 className="view-header">Description:</h1>
+			</div>
+			<div className="detail">
+				<div className="description">
+					<p className="paragraph">{props.hotel_description}</p>
+					<ul className="list">
+						<li className="list__item">Close to the beach</li>
+						<li className="list__item">Breakfast included</li>
+						<li className="list__item">Free airport shuttle</li>
+						<li className="list__item">Free wifi in all rooms</li>
+						<li className="list__item">Air conditioning and heating</li>
+						<li className="list__item">Pets allowed</li>
+						<li className="list__item">We speak all languages</li>
+						<li className="list__item">Perfect for families</li>
+					</ul>
+					<div className="recommend">
+						<p className="recommend__count">
+							Lucy and 3 other friends recommend this hotel
+						</p>
+						<div className="recommend__friends">
+						</div>
+					</div>
+				</div>
+				<div className="user-reviews">
+					<figure className="review">
+						<blockquote className="review__text">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga doloremque architecto dicta
+							animi, totam, itaque officia ex.
+						</blockquote>
+						<figcaption className="review__user">
+							<div className="review__user-box">
+								<p className="review__user-name">Nick Smith</p>
+								<p className="review__user-date">Feb 23rd, 2017</p>
+							</div>
+							<div className="review__rating">7.8</div>
+						</figcaption>
+					</figure>
+
+					<figure className="review">
+						<blockquote className="review__text">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga doloremque architecto dicta
+							animi.
+						</blockquote>
+						<figcaption className="review__user">
+							<div className="review__user-box">
+								<p className="review__user-name">Mary Thomas</p>
+								<p className="review__user-date">Sept 13th, 2017</p>
+							</div>
+							<div className="review__rating">9.3</div>
+						</figcaption>
+					</figure>
+
+					<button className="btn btn--inline">Show all <span>&rarr;</span></button>
+				</div>
+			</div>
+			<HotelFacilities {...props} withDescription={true} />
+		</div>
+	)
+}
