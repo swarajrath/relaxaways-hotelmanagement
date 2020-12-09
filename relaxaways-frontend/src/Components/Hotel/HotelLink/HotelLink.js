d2cee320dea83bd6bcf325fbffd5191a73dc9100
Swaraj Rath <gulal009@gmail.com>
2020-12-09 18:53:44 +0100

    Link to hotel

---
+++ relaxaways-frontend/src/Components/Hotel/HotelLink/HotelLink.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+
+
+const HotelLink = (data) => (
+	<div className="hotel__name">
+		<NavLink to={"/hotel/" + data.id} className="hotel__link">{data.hotelName}</NavLink>
+	</div>
+);
+
+export default HotelLink;
